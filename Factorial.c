e65377e1dfaba0ceef3acb8dc50e7e0eaf1d8774
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2021-10-03 20:48:48 +0530

    Create Factorial.c
    This have Some mistake , fix that

---
+++ Factorial.c
@@ -0,0 +1,20 @@
+#include <iostreame>
+using namespace std;
+int main(void)
+{
+ int no;
+ long fac;
+
+ cout << "Enter a Number : ";
+ cin >> fac;
+
+ fac = 2;
+
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * w;
+  }
+
+ cout << "Factorial of " << no <<" is " fac <<endl 
+ 
+}
