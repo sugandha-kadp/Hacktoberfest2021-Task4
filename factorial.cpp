76d317f91846e239558d67e82962e916bfd37630
Mayukh Saha <79632273+Mayukh089@users.noreply.github.com>
2021-10-06 15:22:37 +0530

    Update and rename factorial.c to factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostreame>
+#include <iostream>
+using namespace std;
+int main(void)
+{
+ int no;
+ long fac;
+
+ cout << "Enter a Number : ";
+ cin >> fac;
+ cin >> no;
+
+ fac = 2;
+ fac = 1;
+
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * w;
+    fac = fac * r;
+  }
+
+ cout << "Factorial of " << no <<" is " fac <<endl 
+ cout << "Factorial of " << no <<" is " << fac <<endl ;
+
+ return 0;
+}
