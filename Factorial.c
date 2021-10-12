ce1283f23d365c8d9892e13ddb588cba2eb568ee
Prasad Chathuranga <46666566+Prasad-Chathuranga@users.noreply.github.com>
2021-10-12 19:01:20 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,20 +1,29 @@
-#include <iostreame>
-using namespace std;
+#include <stdio.h>
+
+
 int main(void)
 {
- int no;
- long fac;
+
+  int no;
+  long fac;
 
  cout << "Enter a Number : ";
  cin >> fac;
+  printf("Enter a Number : ");
+  scanf("%d",&no);
 
  fac = 2;
+  fac = 1;
 
   for (int r=no; r >= 1; r--)
   {
     fac = fac * w;
   }
+  for(int r = 2 ; r <= no ; r++){
+    fac *= r;
+  }  
 
  cout << "Factorial of " << no <<" is " fac <<endl 
- 
-}
+
+  printf("Factorial of %d is %ld\n", no,fac); 
+  
