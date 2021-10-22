97378453e12653bc2797a75a9d91d6209feb83da
Anubhav Gupta <anubhavguptadec1999@gmail.com>
2021-10-22 12:54:23 +0530

    Update and rename Factorial.c to Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+ long no;
+ long long fac;
+
+ cout << "Enter a Number : ";
+ cin >> no;
+
+ fac = 1;
+
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * r;
+  }
+
+ cout << "Factorial of " << no <<" is "<<fac <<endl;
+ 
+}
